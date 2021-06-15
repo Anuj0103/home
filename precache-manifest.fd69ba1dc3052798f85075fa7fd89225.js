eae0f05076561f852aa5f5db7d6e4f8c8f09d7b0
Anuj0103 <Anuj@0103>
2021-06-15 17:42:07 +0530

    Updates

---
+++ precache-manifest.fd69ba1dc3052798f85075fa7fd89225.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d689ff256926a168a645cfc7fe144f1b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a5139f372d488f9a2fcc",
+    "url": "/home/static/css/main.d11511b8.chunk.css"
+  },
+  {
+    "revision": "1b3b64b6fd50e313aae6",
+    "url": "/home/static/js/2.ce1bf75a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.ce1bf75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5139f372d488f9a2fcc",
+    "url": "/home/static/js/main.c4fe2fae.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "283a98283692bc421011bfad0d4cd81a",
+    "url": "/home/static/media/img.283a9828.jpg"
+  },
+  {
+    "revision": "39015105acc97518372711296a527b72",
+    "url": "/home/static/media/resume.39015105.pdf"
+  }
+]);
