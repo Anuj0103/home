5290564b6164e25ebf8a640a0fc5fc767762ec2a
Anuj0103 <yanujdhingra01@gmail.com>
2023-05-20 14:42:11 +0530

    Updates

---
+++ precache-manifest.93d8181217c27bea4e3c0ac78f53c57c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8ca9e9be3eb68f9f7f1a38923e61a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48ae7f76b2f20f513746",
+    "url": "/static/css/main.d11511b8.chunk.css"
+  },
+  {
+    "revision": "1b3b64b6fd50e313aae6",
+    "url": "/static/js/2.ce1bf75a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.ce1bf75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ae7f76b2f20f513746",
+    "url": "/static/js/main.49d2d72e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c14ce24a62690c28dffa45081a7226f2",
+    "url": "/static/media/img.c14ce24a.jpg"
+  },
+  {
+    "revision": "6c5f1757dec74ba14b112675fa9c0d7d",
+    "url": "/static/media/resume.6c5f1757.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.32bd94fccd4ce6e5274102fdaa6d000f.js"
+  "/precache-manifest.93d8181217c27bea4e3c0ac78f53c57c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
